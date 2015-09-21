ab2f82ce869af3d79494bbcd919a06491f10d405
Dave Leach <dave@dmleach.com>
2015-09-21 08:00:38 -0400

    Created home controller, added navigation menu and portfolio building methods

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    protected function calcNavMenu()
+    {
+        return array (
+            array (
+                "Caption" => "Home",
+                "Route" => "home"
+            ),
+            array (
+                "Caption" => "Resume",
+                "Route" => "resume"
+            ),
+            array (
+                "Caption" => "Portfolio",
+                "Route" => "portfolio"
+            ),
+            array (
+                "Caption" => "Blog",
+                "Route" => "blog"
+            ),
+        );
+    }
+
+    public function displayAction()
+    {
+        return $this->render(
+            "base.html.twig",
+            array (
+                "body" => "Hello!",
+                "navMenu" => $this->calcNavMenu(),
+                "portfolio" => array (
+                    "heading" => "Current Projects",
+                    "projects" => $this->calcProjects()
+                ),
+                "subtitle" => "Delphi and PHP programmer in Atlanta, Georgia",
+                "title" => "Dave Leach"
+            )
+        );
+    }
+
+    protected function calcProjects()
+    {
+        return array (
+            0 => array (
+                "name" => "daveleach.work",
+                "description" => "A website for my professional projects and
+                    blog, as well as a sandbox to learn new technologies. You
+                    might be looking at it and this very moment!",
+                "route" => "project_daveleach"
+            )
+        );
+    }
+}
+
+?>
