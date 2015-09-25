9008da2657ea344a0d871e26dc6bc51715973a3b
Dave Leach <dave@dmleach.com>
2015-09-25 17:52:05 -0400

    Created technology entity. Project now loads related tech from database

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -4,11 +4,11 @@ namespace AppBundle\Controller;
 
 class ProjectController extends BaseController
 {
-    private $ShortName = null;
+    private $Project = null;
 
     public function displayAction($shortname = null)
     {
-        $this->ShortName = $shortname;
+        $this->fetchProject($shortname);
         return parent::displayAction();
     }
 
@@ -20,13 +20,29 @@ class ProjectController extends BaseController
     protected function displayParameters()
     {
         return array (
-            "name" => $this->ShortName
+            "project" => $this->Project
         );
     }
 
     protected function displayTemplate()
     {
         return 'project.html.twig';
+    }
+
+    protected function fetchProject($shortname)
+    {
+        $QueryResult = $this->Project = $this->getDoctrine()
+            ->getRepository("AppBundle:Project")
+            ->findBy(array ("shortname" => $shortname));
+
+        if (count($QueryResult) == 1) {
+            $Project = $QueryResult[0];
+        } else {
+            $Project = null;
+        };
+
+        dump($Project);
+        $this->Project = $Project;
     }
 }
 
